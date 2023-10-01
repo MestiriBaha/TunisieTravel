3dea74fdb3ea0105d44f262001df705a9965f1c8
MestiriBaha <mohamedbaha.mestiri@esprit.tn>
2023-10-01 20:00:05 +0100

    recovered file

---
+++ Domain/Advertisement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Advertisement
+    {
+        public string Location { get; set;  }
+        public int Days { get; set;  }
+
+        public double Price { get; set; }
+        public double Discount { get; set; }    
+        public DateTime DepartureDate { get; set;  }
+        
+    }
+}
